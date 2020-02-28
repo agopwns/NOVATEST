57bd66bdddf826cea8073c3dd8edbfbb41380fdd
SeongJun Ha <agopwns@naver.com>
2020-02-29 01:39:46 +0900

    올림픽 정보 화면

---
+++ app/src/main/java/com/example/novatrip/ScheduleOlympicInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.novatrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScheduleOlympicInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_olympic_info);
+    }
+}
